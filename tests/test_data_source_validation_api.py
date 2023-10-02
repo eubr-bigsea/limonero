ac440088fa16309b728c56c37ce8d84f837feaba
ericksunclair <ericksunclair@hotmail.com>
2023-10-02 17:07:21 -0300

    validation fail not authorized test

--- tests/test_data_source_validation_api.py
+++ tests/test_data_source_validation_api.py
@@ -2,6 +2,21 @@
 from limonero.models import *
 from flask import current_app
 
+def test_validation_fail_not_authorized(client):
+    tests = [
+        lambda: client.get('/datasources/validations'),
+        lambda: client.post('/datasources/validations/executions'),
+        lambda: client.get('/datasources/validations/1'),
+        lambda: client.patch('/datasources/validations/executions/1'),
+    ]
+    for i, test in enumerate(tests):
+        rv = test()
+        assert 401 == rv.status_code, \
+            f'Test {i}: Incorrect status code: {rv.status_code}'
+        resp = rv.json
+        assert resp['status'] == 'ERROR', f'Test {i}: Incorrect status'
+        assert 'Thorn' in resp['message'], f'Test {i}: Incorrect message'
+
 def test_validation_list(client):
     headers={'X-Auth-Token': str(client.secret)}
     rv = client.get('/datasources/validations', headers=headers)
@@ -12,7 +27,7 @@ def test_validation_list(client):
     with current_app.app_context():
         default_validation = DataSourceValidation.query.order_by(
             DataSourceValidation.description).first()
-    
+    breakpoint()
     assert resp['data'][0]['id'] == default_validation.id
     assert resp['data'][0]['description'] == default_validation.description
     assert resp['data'][0]['type'] == default_validation.type
